862b1bb9495bbaf6b19fc10fe0e38b38644dc7fe
prataparun1592 <prataparun1592@gmail.com>
2021-06-02 09:07:42 +0530

    new project commit

---
+++ src/main/java/com/newwebapp/newwebapp/NewwebappApplication.java
@@ -0,0 +1,13 @@
+package com.newwebapp.newwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewwebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/newwebapp/newwebapp/NewwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newwebapp.newwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
